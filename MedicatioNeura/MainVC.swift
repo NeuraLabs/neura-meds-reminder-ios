a5901c05172db14032a41a3dd40f8aa9a8a456cf
Youval Vaknin <youval@theneura.com>
2017-01-01 00:18:32 +0200

    first commit! yay! generalized the pill reminder view in a xib file constraints for iPhone 5 are not yet perfect :(

--- MedicatioNeura/MainVC.swift
+++ MedicatioNeura/MainVC.swift
@@ -13,21 +13,16 @@ class MainVC: UIViewController {
     
     //MARK: Properties
     
-    @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var progressView: UIView!
-    
     @IBOutlet weak var mainView: UIView!
     @IBOutlet weak var sideBarContainer: UIView!
     @IBOutlet weak var welcomeLabel: UILabel!
-    @IBOutlet weak var morningTookLabel: CustomLabel!
-    @IBOutlet weak var morningMissedLabel: CustomLabel!
-    @IBOutlet weak var eveningTookLabel: CustomLabel!
-    @IBOutlet weak var eveningMissedLabel: CustomLabel!
-    @IBOutlet weak var takeTookLabel: CustomLabel!
-    @IBOutlet weak var takeMissedLabel: CustomLabel!
     
     @IBOutlet weak var showSideBarButton: UIButton!
     
+    @IBOutlet weak var stackView: UIStackView!
+    var MorningView: PillReminderView!
+    var EveningView: PillReminderView!
+    var PillBoxView: PillReminderView!
     
     //MARK: Lifecycle Functions
     override func viewDidLoad() {
@@ -45,33 +40,26 @@ class MainVC: UIViewController {
         NotificationCenter.default.removeObserver(self)
     }
     
-    
-    
     func willEnterForeground() {
         welcomeLabel.text = NeuraSDKManager.manager.greetingMessage()
         
         setCountLabels()
         
-        if NeuraSDKManager.manager.eveningPillsProgress() > 2 {
-            self.progressView.alpha = 0
-            
-        } else {
-            self.progressBar.progress = Float(NeuraSDKManager.manager.eveningPillsProgress()) / 2
+        if NeuraSDKManager.manager.eveningPillsProgress() <= 2 {
+            self.EveningView.updateView(progressValue: Float(NeuraSDKManager.manager.eveningPillsProgress()) / 2)
         }
     }
     
-    
     func setCountLabels() {
-        self.morningTookLabel.text = UserDefaults.standard.string(forKey: kMorningTookCount)
-        self.eveningTookLabel.text = UserDefaults.standard.string(forKey: kEveningTookCount)
-        self.takeTookLabel.text = UserDefaults.standard.string(forKey: kPillboxTookCount)
+        self.MorningView = PillReminderView.instanceFromNib(imageName:"morningiconsmall", headline: "Morning Pills Reminder", subHeadline: "Get reminded to take your pills when you wake up", takenCount: UserDefaults.standard.string(forKey: kMorningTookCount), missedCount: UserDefaults.standard.string(forKey: kMorningMissedCount))
+        self.EveningView = PillReminderView.instanceFromNib(imageName:"eveningiconsmall", headline: "Evening Pills Reminder", subHeadline: "Get reminded to take your pills when you go to sleep", takenCount: UserDefaults.standard.string(forKey: kEveningTookCount), missedCount: UserDefaults.standard.string(forKey: kEveningMissedCount))
+        self.PillBoxView = PillReminderView.instanceFromNib(imageName:"pillboxiconsmall", headline: "Med Box Reminder", subHeadline: "Get reminded to take your pillbox with you when you leave home", takenCount: UserDefaults.standard.string(forKey: kPillboxTookCount), missedCount: UserDefaults.standard.string(forKey: kPillboxMissedCount))
         
-        self.morningMissedLabel.text = UserDefaults.standard.string(forKey: kMorningMissedCount)
-        self.eveningMissedLabel.text = UserDefaults.standard.string(forKey: kEveningMissedCount)
-        self.takeMissedLabel.text = UserDefaults.standard.string(forKey: kPillboxMissedCount)
-    }
-    
-    
+        self.stackView.addArrangedSubview(self.MorningView)
+        self.stackView.addArrangedSubview(self.EveningView)
+        self.stackView.addArrangedSubview(self.PillBoxView)
+        
+        }
     
     func isSideBarRevealed() -> Bool {
         return self.mainView.frame.origin.x < 0
@@ -95,21 +83,15 @@ class MainVC: UIViewController {
         }
     }
     
-    
-    
     //MARK: IBAction Functions
     @IBAction func showSideBarButtonPressed(_ sender: Any) {
         self.toggleSideBar()
     }
-    
     
     @IBAction func onSwipedRight(_ sender: Any) {
         if self.isSideBarRevealed() {
             self.toggleSideBar()
         }
     }
-    
-    
-    
 }
 
